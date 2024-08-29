28ea0ed21e55e82611d1bdb968c99cb17042479f
KienNguyen <letvn.com@gmail.com>
2024-08-29 19:12:19 +0700

    update new code error

--- src/Config/ErrorCodes.php
+++ src/Config/ErrorCodes.php
@@ -7,24 +7,28 @@ class ErrorCodes
     public static function getErrorMessages(): array
     {
         return [
-            '01' => 'ACCOUNT_EXISTED - Tài khoản đã tồn tại.',
-            '02' => 'ACCOUNT_NUMBER_INVALID - Số tài khoản không hợp lệ.',
-            '03' => 'CAN_NOT_CREATE_ACCOUNT - Không thể tạo tài khoản.',
-            '04' => 'ACCOUNT_TYPE_INVALID - Loại tài khoản không hợp lệ.',
-            '05' => 'ACCOUNT_NO_PURE_INVALID - Cấu hình khởi tạo tài khoản không hợp lệ.',
-            '06' => 'ACCOUNT_NO_CONFLICT - Thông tin khởi tạo tài khoản có xung đột trong request.',
-            '07' => 'ACCOUNT_NAME_INVALID - Tên tài khoản không hợp lệ.',
-            '10' => 'PARTNER_NOT_FOUND - Không tìm thấy tên đối tác.',
-            '11' => 'MERCHANT_NOT_FOUND - Không tìm thấy tên merchant.',
-            '12' => 'PARTNER_OR_MERCHANT_INVALID - Đối tác không hợp lệ.',
-            '13' => 'PARTNER_ACCOUNT_INVALID - Tài khoản đối tác không hợp lệ.',
-            '14' => 'PARTNER_ACCOUNT_NOT_FOUND - Không tìm thấy tài khoản đối tác.',
-            '15' => 'FIXED_AMOUNT_INVALID - Số tiền cần thu hộ không hợp lệ.',
-            '16' => 'PARTNER_MAINTAIN_ERR - Lỗi bảo trì tài khoản đối tác.',
-            '20' => 'REQUEST_ID_INVALID - Request Id không hợp lệ.',
-            '22' => 'REQUEST_ID_DUPLICATE - Request Id bị trùng.',
-            '98' => 'EXCEPTION - Lỗi hệ thống.',
-            '99' => 'ERROR_UNKNOWN - Lỗi không xác định.'
+            'API000' => 'Thành công',
+            'QR-001' => 'Không tìm thấy thông tin phù hợp',
+            'QR-002' => 'Có lỗi trong quá trình đóng tài khoản',
+            'QR-004' => 'Giao dịch hạch toán chưa rõ kết quả',
+            'QR-005' => 'Giao dịch hạch toán bị lỗi',
+            'QR-006' => 'Chưa cấu hình tài khoản thanh toán cho Bưu điện tỉnh',
+            'QR-007' => 'Số tài khoản đã tồn tại ở một bưu cục khác',
+            'QR-008' => 'Tên tài khoản đã tồn tại ở một bưu cục khác',
+            'QR-009' => 'Tài khoản đã ngừng hoạt động',
+            'QR-010' => 'Đã tồn tại mã QR của nhóm được chọn',
+            'QR-011' => 'Giao dịch đang tồn tại yêu cầu xử lý sự vụ',
+            'QR-012' => 'Giao dịch không có yêu cầu xử lý sự vụ',
+            'QR-013' => 'Dữ liệu không hợp lệ',
+            'QR-014' => 'Merchant chưa được cấu hình staffId',
+            'QR-015' => 'Không tìm thấy thông tin tổ chức',
+            'QR-016' => 'Tên tài khoản không hợp lệ',
+            'QR-017' => 'Loại tài khoản không hợp lệ',
+            'QR-018' => 'Số tiền không hợp lệ',
+            'QR-019' => 'RequestID không hợp lệ',
+            'QR-020' => 'RequestID bị trùng',
+            'QR-098' => 'Lỗi hệ thống',
+            'QR-099' => 'Lỗi không xác định'
         ];
     }
 }
